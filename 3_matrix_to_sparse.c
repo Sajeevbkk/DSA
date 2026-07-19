6577820a33b952e5e0bcc779bdc1892abde201b4
Sangeeth <sajeevbkk@gmail.com>
2026-07-19 18:26:15 +0530

    Efficiency Upgrade 3rd matrix to sparse

--- 3_matrix_to_sparse.c
+++ 3_matrix_to_sparse.c
@@ -12,13 +12,11 @@ int main() {
     for (int i = 0; i < row; i++) 
         for (int j = 0; j < col; j++) {
             scanf("%d", &matrix[i][j]);
-            if (matrix[i][j] != 0) number_non_zero++;
     }
 
     int sparse[number_non_zero+1][3], r = 1;
     sparse[0][0] = row;
     sparse[0][1] = col;
-    sparse[0][2] = number_non_zero;
 
     for (int i = 0; i < row; i++) 
         for (int j = 0; j < col; j++) 
@@ -27,7 +25,10 @@ int main() {
                 sparse[r][1] = j;
                 sparse[r][2] = matrix[i][j];
                 r++;
+                number_non_zero++;
             }
+
+    sparse[0][2] = number_non_zero;
 
     for (int i = 0; i <= number_non_zero; i++) {
         printf("(");
